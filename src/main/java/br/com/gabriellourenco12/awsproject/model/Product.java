40f94650d72566b0e7512142cbf4370a52f5b3da
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2022-07-29 01:18:25 -0300

    criacao da classe que define a tabela de produtos

---
+++ src/main/java/br/com/gabriellourenco12/awsproject/model/Product.java
@@ -0,0 +1,28 @@
+package br.com.gabriellourenco12.awsproject.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"code"})})
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(length = 32, nullable = false)
+    private String name;
+
+    @Column(length = 24, nullable = false)
+    private String model;
+
+    @Column(length = 8, nullable = false)
+    private String code;
+
+    private  float price;
+}
