b49bc296251da15949bae7a74b8db384c2decc38
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2022-08-02 01:28:46 -0300

    teste do publisher de eventos localmente

--- src/main/java/br/com/gabriellourenco12/awsproject/config/SnsConfig.java
+++ src/main/java/br/com/gabriellourenco12/awsproject/config/SnsConfig.java
@@ -7,8 +7,10 @@ import com.amazonaws.services.sns.model.Topic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("!local")
 public class SnsConfig {
     @Value("${aws.region}")
     private String awsRegion;
---
+++ src/main/java/br/com/gabriellourenco12/awsproject/config/local/SnsCreate.java
@@ -0,0 +1,44 @@
+package br.com.gabriellourenco12.awsproject.config.local;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.model.CreateTopicRequest;
+import com.amazonaws.services.sns.model.Topic;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("local")
+@Slf4j
+public class SnsCreate {
+    private final String productEventsTopic;
+    private final AmazonSNS snsClient;
+
+    public SnsCreate() {
+        this.snsClient = AmazonSNSClient.builder()
+                .withEndpointConfiguration(new AwsClientBuilder
+                        .EndpointConfiguration("http://localhost:4566", Regions.US_EAST_1.getName()))
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .build();
+
+        CreateTopicRequest createTopicRequest = new CreateTopicRequest("product-events");
+        this.productEventsTopic = this.snsClient.createTopic(createTopicRequest).getTopicArn();
+
+        log.info("SNS topic ARN: {}", this.productEventsTopic);
+    }
+
+    @Bean
+    public AmazonSNS snsClient() {
+        return this.snsClient;
+    }
+
+    @Bean(name = "productEventsTopic")
+    public Topic snsProductEventsTopic() {
+        return new Topic().withTopicArn(productEventsTopic);
+    }
+}
