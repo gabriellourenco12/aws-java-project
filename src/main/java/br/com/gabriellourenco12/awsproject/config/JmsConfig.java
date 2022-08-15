53746632696d3a9d4d7a86dc965ae2c3a9709278
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2022-08-15 01:33:20 -0300

    Configurando a aplicação para acessar o SQS

---
+++ src/main/java/br/com/gabriellourenco12/awsproject/config/JmsConfig.java
@@ -0,0 +1,44 @@
+package br.com.gabriellourenco12.awsproject.config;
+
+import com.amazon.sqs.javamessaging.ProviderConfiguration;
+import com.amazon.sqs.javamessaging.SQSConnectionFactory;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.support.destination.DynamicDestinationResolver;
+
+import javax.jms.Session;
+
+@Configuration
+@EnableJms
+@Profile("!local")
+public class JmsConfig {
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    private SQSConnectionFactory sqsConnectionFactory;
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+        sqsConnectionFactory = new SQSConnectionFactory(
+                new ProviderConfiguration(),
+                AmazonSQSClientBuilder.standard()
+                        .withRegion(awsRegion)
+                        .withCredentials(new DefaultAWSCredentialsProviderChain())
+                        .build());
+
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(sqsConnectionFactory);
+        factory.setDestinationResolver(new DynamicDestinationResolver());
+        factory.setConcurrency("2");
+        factory.setSessionAcknowledgeMode(Session.CLIENT_ACKNOWLEDGE);
+
+        return factory;
+    }
+}
---
+++ src/main/java/br/com/gabriellourenco12/awsproject/config/S3Config.java
@@ -0,0 +1,22 @@
+package br.com.gabriellourenco12.awsproject.config;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    @Bean
+    public AmazonS3 amazonS3Client() {
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(awsRegion)
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .build();
+    }
+}
