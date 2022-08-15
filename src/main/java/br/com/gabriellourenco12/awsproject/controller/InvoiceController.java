adc14748d5d0da3e7dc87a155a8ba6eaec864cb7
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2022-08-15 02:30:35 -0300

    Criando o controller para solicitar a URL pré-assinada

---
+++ src/main/java/br/com/gabriellourenco12/awsproject/controller/InvoiceController.java
@@ -0,0 +1,50 @@
+package br.com.gabriellourenco12.awsproject.controller;
+
+import br.com.gabriellourenco12.awsproject.model.UrlResponse;
+import com.amazonaws.HttpMethod;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/invoices")
+public class InvoiceController {
+
+    @Value("${aws.s3.bucket.invoice.name}")
+    private String bucketName;
+
+    private AmazonS3 amazonS3;
+
+    @Autowired
+    public InvoiceController(AmazonS3 amazonS3) {
+        this.amazonS3 = amazonS3;
+    }
+
+    @PostMapping
+    public ResponseEntity<UrlResponse> createInvoiceUrl() {
+        UrlResponse urlResponse = new UrlResponse();
+        Instant expirationTime = Instant.now().plus(Duration.ofMinutes(5));
+        String processId = UUID.randomUUID().toString();
+
+        GeneratePresignedUrlRequest generatePresignedUrlRequest =
+                new GeneratePresignedUrlRequest(bucketName, processId)
+                        .withMethod(HttpMethod.PUT)
+                        .withExpiration(Date.from(expirationTime));
+
+        urlResponse.setExpirationTime(expirationTime.getEpochSecond());
+        urlResponse.setUrl(amazonS3.generatePresignedUrl(generatePresignedUrlRequest).toString());
+
+        return new ResponseEntity<>(urlResponse, HttpStatus.OK);
+    }
+}
